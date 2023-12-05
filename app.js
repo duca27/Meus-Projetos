3d56e91a2f53f62140ca4b230e6c81a5a7664935
duca27 <152569671+duca27@users.noreply.github.com>
2023-12-04 21:01:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+alert('Saudações terráqueos');
+let numeroMáximo = 9000
+let numeroSecreto = parseInt (Math.random() * numeroMáximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMáximo}`);
+    if (chute == numeroSecreto) {
+        break;
+    } else { 
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}.`);
+        } else {
+            alert(`O número secreto é maior que ${chute}.`);
+        }
+        tentativas++;
+    } 
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
